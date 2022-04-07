881314f8333b4b20deed12337fe54d4ed488fb9a
DanSmaR <dansm86@outlook.com>
2022-04-07 19:18:52 -0300

    logica pega o menor valor do array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
@@ -1,31 +1,31 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// 1 Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ; 
+// 1 Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-for(const number of numbers) {
+for (const number of numbers) {
   console.log(number);
 }
-console.log('****************************************************');
+console.log("****************************************************");
 
 // *******************************************************************************
 
-// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
+// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let sumArray = 0;
 
-for(const number of numbers) {
+for (const number of numbers) {
   sumArray += number;
 }
 
-console.log('Soma dos elementos: ' + sumArray);
-console.log('****************************************************');
+console.log("Soma dos elementos: " + sumArray);
+console.log("****************************************************");
 
 // ********************************************************************************
 
 // 3 Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 const mediaArray = sumArray / numbers.length;
-console.log('Media do Array numbers: ' + mediaArray);
-console.log('****************************************************');
+console.log("Media do Array numbers: " + mediaArray);
+console.log("****************************************************");
 
 // ********************************************************************************
 
@@ -33,28 +33,28 @@ console.log('****************************************************');
 4 Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
  */
 
-if (mediaArray > 20) console.log('Valor maior que 20');
-else console.log('valor menor ou igual a 20');
-console.log('****************************************************');
+if (mediaArray > 20) console.log("Valor maior que 20");
+else console.log("valor menor ou igual a 20");
+console.log("****************************************************");
 // ****************************************************************************
 
-// 5 Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
+// 5 Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
-let higherValue = 0;
+let higherValue = numbers[0];
 
-for(let i = 0; i < numbers.length; i += 1) {
-  if (numbers[i] > higherValue) {
-    higherValue = numbers[i];
+for (let i = 0; i < numbers.length - 1; i += 1) {
+  if (numbers[i + 1] > higherValue) {
+    higherValue = numbers[i + 1];
   }
 }
 
-console.log('Maior valor: ' + higherValue);
-console.log('****************************************************');
+console.log("Maior valor: " + higherValue);
+console.log("****************************************************");
 
 // ********************************************************************************
 
 /**
- * 6 Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"; 
+ * 6 Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
  */
 
 let oddNumbers = 0;
@@ -65,8 +65,25 @@ for (const number of numbers) {
   }
 }
 
-if (oddNumbers !== 0) console.log('números ímpares: ' + oddNumbers);
-else console.log('nenhum valor ímpar encontrado');
-console.log('****************************************************');
+if (oddNumbers !== 0) console.log("números ímpares: " + oddNumbers);
+else console.log("nenhum valor ímpar encontrado");
+console.log("****************************************************");
+
+// ********************************************************************************
+
+/**
+ * 7 Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+ */
+
+let lowerValue = numbers[0];
+
+for (let i = 0; i < numbers.length - 1; i += 1) {
+  if (numbers[i + 1] < lowerValue) {
+    lowerValue = numbers[i + 1];
+  }
+}
+
+console.log("Menor valor: " + lowerValue);
+console.log("****************************************************");
 
 // ********************************************************************************

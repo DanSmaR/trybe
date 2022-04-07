f795f00d648aea7d4e087179d2124750abfd5d9f
DanSmaR <dansm86@outlook.com>
2022-04-07 18:49:36 -0300

    Logica para pegar o maior valor do array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
@@ -11,7 +11,7 @@ console.log('****************************************************');
 
 // 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
 
-let sumArray = 0
+let sumArray = 0;
 
 for(let number of numbers) {
   sumArray += number;
@@ -27,4 +27,28 @@ const mediaArray = sumArray / numbers.length;
 console.log('Media do Array numbers: ' + mediaArray);
 console.log('****************************************************');
 
-// *********************************************************************************
+// ********************************************************************************
+
+/**
+4 Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+ */
+
+if (mediaArray > 20) console.log('Valor maior que 20');
+else console.log('valor menor ou igual a 20');
+console.log('****************************************************');
+// ****************************************************************************
+
+// 5 Utilizando for , descubra qual o maior valor contido no array e imprima-o; 
+
+let higherValue = 0;
+
+for(let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > higherValue) {
+    higherValue = numbers[i];
+  }
+}
+
+console.log('Maior valor: ' + higherValue);
+console.log('****************************************************');
+
+// ********************************************************************************

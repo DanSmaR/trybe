40d7bcf8d5f22ffc451a3de12125e67e9c560434
DanSmaR <dansm86@outlook.com>
2022-04-07 09:38:53 -0300

    Conversor de porcentagem para Letras

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_1-javascript-primeiros-passos/js-first-steps.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_1-javascript-primeiros-passos/js-first-steps.js
@@ -77,7 +77,7 @@ if (anguloTriangulo1 > 0 && anguloTriangulo2 > 0 && anguloTriangulo3 > 0) {
     - Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
     - Exemplo: bishop (bispo) -> diagonals (diagonais)
 */
-
+/*
 let pecaXadrez = "Cavalo";
 pecaXadrez = pecaXadrez.toLowerCase();
 
@@ -103,4 +103,37 @@ switch (pecaXadrez) {
 
  default:
   break;
+}
+*/
+
+// **************************************************************************************************************************************
+
+/**
+ * 7 Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras: 
+ * 
+    Porcentagem >= 90 -> A
+    Porcentagem >= 80 -> B
+    Porcentagem >= 70 -> C
+    Porcentagem >= 60 -> D
+    Porcentagem >= 50 -> E
+    Porcentagem < 50 -> F
+    O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+ */
+
+const porcentagem = 77;
+
+if (porcentagem > 100 || porcentagem < 0) {
+  console.error('Erro. Deve ser maior que 0 e menor que 100');
+} else if (porcentagem >= 90) {
+  console.log('A');
+} else if (porcentagem >= 80) {
+  console.log('B');
+} else if (porcentagem >= 70) {
+  console.log('C');
+} else if (porcentagem >= 60) {
+  console.log('D');
+} else if (porcentagem >= 50) {
+  console.log('E');
+} else {
+  console.log('F');
 }

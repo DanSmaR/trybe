624150b7c20de76582a5f1b11673da50440724c4
DanSmaR <dansm86@outlook.com>
2022-04-07 18:29:08 -0300

    Calculo da soma e media do array numbers

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_2-javascript-array-e-loop-for/array-loop.js
@@ -5,5 +5,26 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(let number of numbers) {
   console.log(number);
 }
+console.log('****************************************************');
 
 // *******************************************************************************
+
+// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; 
+
+let sumArray = 0
+
+for(let number of numbers) {
+  sumArray += number;
+}
+
+console.log('Soma dos elementos: ' + sumArray);
+console.log('****************************************************');
+
+// ********************************************************************************
+
+// 3 Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+const mediaArray = sumArray / numbers.length;
+console.log('Media do Array numbers: ' + mediaArray);
+console.log('****************************************************');
+
+// *********************************************************************************

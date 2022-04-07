69b9b9e1f36c8993f016197adffbe5905e5d147c
DanSmaR <dansm86@outlook.com>
2022-04-07 00:25:12 -0300

    logica q retorna o maior de 3 num

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_1-javascript-primeiros-passos/js-first-steps.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4_1-javascript-primeiros-passos/js-first-steps.js
@@ -1,5 +1,5 @@
 //Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para: 
-
+/*
 const a = 12;
 const b = 34;
 
@@ -8,15 +8,32 @@ let subtracao = a - b;
 let multiplicacao = a * b;
 let divisao = a / b;
 let modulo = a % b;
-
+*/
 //*************************************************************************************************************************** */
 
 // Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
-const num1 = 35;
-const num2 = 78;
+/*
+const num1 = 78;
+const num2 = 77;
 
 if (num1 > num2) {
   console.log('O maior eh ' + num1);
 } else if (num2 > num1) {
   console.log('O maior eh ' + num2);
 } else console.log('Os numeros sao iguais')
+*/
+//******************************************************************************************************************* */
+
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+/*
+const numA = 12;
+const numB = 16;
+const numC = 15;
+
+if (numA > numB || numA > numC) {
+  console.log('O maior eh ' + numA);
+} else if (numB > numC) {
+  console.log('O maior eh ' + numB);
+} else console.log('O maior eh ' + numC);
+*/
+// *************************************************************************************************************************
